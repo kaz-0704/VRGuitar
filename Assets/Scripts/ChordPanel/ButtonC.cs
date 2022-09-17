b6ba65505cb1454fe21e65d06cb022f67734b05f
kaz-0704 <okk8578@gmail.com>
2022-09-17 15:21:52 +0900

    メトロノームのためのスクリプトを追加

--- Assets/Scripts/ChordPanel/ButtonC.cs
+++ Assets/Scripts/ChordPanel/ButtonC.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CButton : MonoBehaviour
+namespace VRGuitar
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ButtonC : MonoBehaviour
     {
-        
-    }
+        public StringCollider stringCollider;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnClick()
+        {
+            Debug.Log("Pushing" + this.name);  // ÉçÉOÇèoóÕ
+            stringCollider.chord = Chords.C;
+        }
     }
 }
